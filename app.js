1739dad2b0f0d4a8286cae5276ad03d4cf63c9b9
Saikikoko <729897124@qq.com>
2019-01-25 20:10:18 +0800

    入口文件

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const Router = require('./router/router');
+const Router = require('./router/user');
+const Admin = require('./router/admin');
+const Api = require('./router/api');
+const session = require('express-session');
+const Mongosession = require('connect-mongo')(session);
 const app = express();
 
 
@@ -15,9 +19,23 @@ app.use(express.static(__dirname + '/public'));
 //设置模板引擎
 app.set('view engine','ejs');
 
+app.use(session({
+    secret: "puciciliu",
+    rolling: true,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 36e5
+    },
+    store: new Mongosession({
+        url: 'mongodb://localhost:27017/projecct'
+    })
+}))
 
 //配置路由模块
 app.use('/',Router);
+app.use('/admin',Admin);
+app.use('/api',Api);
 
 //监听端口号
 app.listen(3000,()=>{
